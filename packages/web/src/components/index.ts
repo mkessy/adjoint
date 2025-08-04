65f8d41707eec92866569dbd7fa57bafa89a0523
mkessy <kokokessy@gmail.com>
2025-08-04 01:57:18 -0700

    Remove deprecated CLI, TodosClient, and server files along with associated documentation. This cleanup eliminates unused code and prepares the codebase for future enhancements.

--- packages/web/src/components/index.ts
+++ packages/web/src/components/index.ts
@@ -3,13 +3,12 @@
  */
 
 // Main components
-export * from "./ObservableCanvas.js"
-export * from "./DataRainVisualizer.js"
-export * from "./NLPSearchBar.js"
-export * from "./CommutativeSquarePreview.js"
+// export * from "./ObservableCanvas.js"
+export * from "./ProvenanceGraph.js"
+export * from "./SchemaGraphVisualizer.js"
 
 // Additional components (to be implemented)
-export * from "./SchemaGraphVisualizer.js"
-export * from "./ProvenanceGraph.js"
-export * from "./MaterializationProgress.js"
 export * from "./AlgebraLibrary.js"
+export * from "./MaterializationProgress.js"
+export * from "./ProvenanceGraph.js"
+export * from "./SchemaGraphVisualizer.js"
